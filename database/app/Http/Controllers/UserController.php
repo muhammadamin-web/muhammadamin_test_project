a9cbe03235b83caffd84e66a37c0a48da3bac1b1
muhammadamin <mirboqijonov@gmail.com>
2021-08-08 10:54:33 +0500

    database

---
+++ database/app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        // $users = DB::table('users')->get();
+        // $user = DB::table('users')->where('name', 'Kaylie Stokes')->value('email');
+        
+
+
+    // echo $user;
+    // $titles = DB::table('users')->pluck('name');
+
+    // foreach ($titles as $title) {
+    //     echo "<li>" . $title . "</li><br>";
+    // }
+
+    // $titles = DB::table('users')->pluck('email', 'name');
+
+    // foreach ($titles as $name => $title) {
+    //     echo "<li>" . $title . $name . "</li>";
+    // }
+
+    DB::table('users')->orderBy('id')->chunk(100, function ($users) {
+        foreach ($users as $user) {
+            echo "<li>" . $user . "</li>";
+        }
+    });
+    }
+    
+}
